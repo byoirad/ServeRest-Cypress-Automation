fd30ae5a962e5ee6e698cd6ee7e8e69103409a1c
Dário B. de Brito <dario.barbosabr@gmail.com>
2025-02-03 14:48:21 -0300

    :wrench: Melhoria na função de valiÃdação da página home

--- cypress/support/actions/admin/index.js
+++ cypress/support/actions/admin/index.js
@@ -1,7 +1,8 @@
-
-
 export const Admin = {
     isVisible: function(text) {
-        cy.get('h1').should('contain', text)
-    }
-} 
+        cy.get('h1')
+            .should('contain', text)
+        cy.get('.lead')
+            .should('have.text', 'Este é seu sistema para administrar seu ecommerce.')
+    },
+}
