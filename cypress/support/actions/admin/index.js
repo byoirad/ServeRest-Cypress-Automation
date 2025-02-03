9a3a1b01b3f8bc9be6a2188850657a0af30deddd
Dário B. de Brito <dario.barbosabr@gmail.com>
2025-02-03 15:40:23 -0300

    :wrench: Meholria na função

--- cypress/support/actions/admin/index.js
+++ cypress/support/actions/admin/index.js
@@ -1,6 +1,6 @@
 export const Admin = {
     isVisible: function(text) {
-        cy.get('h1')
+        cy.get('h1', { timeout: 10000 })
             .should('contain', text)
         cy.get('.lead')
             .should('have.text', 'Este é seu sistema para administrar seu ecommerce.')
