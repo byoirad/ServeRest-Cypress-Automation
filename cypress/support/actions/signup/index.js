e4df307384449a24275d69e5d7d534e9eb239018
Dário B. de Brito <dario.barbosabr@gmail.com>
2025-02-03 14:54:02 -0300

    :wrench: MelCriÃação de funçÃÃões para a página de cadastro

--- cypress/support/actions/signup/index.js
+++ cypress/support/actions/signup/index.js
@@ -1,9 +1,16 @@
-
-
 export const Signup = {
-
     isVisible: function() {
         cy.get('.form').should('be.visible')
-    }
+    },
 
+    fillform: function(user) {
+        cy.get('#nome').type(user.name)
+        cy.get('#email').type(user.email)
+        cy.get('#password').type(user.password)
+        if (user.adm === "true") cy.get('#administrador').click()
+    },
+
+    submit: function(){
+        cy.get('button[type="submit"]').click()
+    },
 }
